9bf3c7d02a4ace2ed2e6e39161246b5640e1f9b9
Noprizal <noprizal@voltras.co.id>
2026-08-11 15:46:45 +0700

    fix

--- components/home/BentoShowcase.tsx
+++ components/home/BentoShowcase.tsx
@@ -19,7 +19,7 @@ export default function BentoShowcase() {
     visible: {
       y: 0,
       opacity: 1,
-      transition: { type: "spring", stiffness: 100, damping: 12 },
+      transition: { type: "spring" as const, stiffness: 100, damping: 12 },
     },
   };
 
